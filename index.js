500a909ac7740fe3ba990c1cbbbdc90cff287c63
kenneth <kennjorothuo@gmail.com>
2024-04-12 14:32:35 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const num3 = 57;
+const num4 = 53;
+const mod = num3 % num4;
+const numbers = [15, 20, 5, 10];
+const max = Math.max(...numbers);
